c08ca83823d825eea09f114f34582643c7465829
Ben Hamouda, Feyez <feyez.ben.hamouda@exxeta.com>
2025-12-09 10:20:34 +0100

    - day5 second try

--- src/main/kotlin/days/Day8.kt
+++ src/main/kotlin/days/Day8.kt
@@ -4,89 +4,75 @@ import kotlin.math.sqrt
 
 open class Day8 : Day(8) {
   data class JunctionBox(val x: Long, val y: Long, val z: Long) {
-    fun distanceTo(to: JunctionBox): Double {
+    fun distanceTo(to: JunctionBox): Long {
       val xDiff = this.x - to.x
       val yDiff = this.y - to.y
       val zDiff = this.z - to.z
 
-      return sqrt(((xDiff * xDiff) + (yDiff * yDiff) + (zDiff * zDiff)).toDouble())
+      return ((xDiff * xDiff) + (yDiff * yDiff) + (zDiff * zDiff))
     }
   }
+
+  data class Connection(var parentIndex: Int, var index: Int, val distance: Long )
 
   fun parse(input: String): List<JunctionBox> = input.lines().map {
     val split = it.split(",")
     JunctionBox(split[0].toLong(), split[1].toLong(), split[2].toLong())
   }
 
+   fun findParent(parents: Map<Int, Int>, toFind: Int): Int {
+    if (parents[toFind] == toFind) return toFind
+
+    parents[toFind] == findParent(parents, parents[toFind]!!)
+    return parents[toFind]!!
+  }
+
+  fun union(parents: MutableMap<Int, Int>, a: Int, b: Int) {
+    val aParent = parents[a]
+    // make b's parent a's parent joining the two circuits
+    parents[b] = aParent!!
+  }
+
   override fun solve1(input: String): Number {
     val junctionBoxes = parse(input)
-    val shortestConnections = mutableListOf<Pair<JunctionBox, JunctionBox>>()
+    val distances = mutableListOf<Connection>()
+    val parents = junctionBoxes.indices.fold(mutableMapOf<Int, Int>()) { acc, it ->
+      acc[it] = it
+      acc
+    }
 
-    for (point in junctionBoxes) {
-      var closest: Pair<JunctionBox, Double>? = null
-      for (compareTo in junctionBoxes) {
+    //calculate distances between all nodes
+    for ((a, point) in junctionBoxes.withIndex()) {
+      for ((b, compareTo) in junctionBoxes.withIndex()) {
         val distance = point.distanceTo(compareTo)
-        when {
-          distance < 1 -> continue
-          closest == null -> closest = compareTo to point.distanceTo(compareTo)
-          distance < closest.second -> closest = compareTo to distance
-        }
+        distances.add(Connection(a, b, distance))
       }
-      shortestConnections.add(point to closest!!.first)
     }
-    val sorted = shortestConnections.sortedBy { it.first.distanceTo(it.second) }
 
-    val filteredDuplicates = mutableListOf<Pair<JunctionBox, JunctionBox>>()
+    val sorted = distances.sortedByDescending { it.distance }
 
-    for (connection in sorted) {
-      if (filteredDuplicates.contains(connection.second to connection.first)) {
+    for (pair in 0..10) {
+      val (a, b, _) = sorted[pair]
+      if (findParent(parents, a) == findParent(parents, b)) continue
+
+      //make sure a and b have the same root
+      union(parents, a, b)
+    }
+
+    val sizes = mutableMapOf<Int, Int>()
+
+    for (node in parents.values) {
+      val root = findParent(parents, node)
+      if (sizes[root] == null) {
+        sizes[root] = 1
         continue
       }
-      filteredDuplicates.add(connection)
+      sizes[root] = sizes[root]!! + 1
     }
 
-    val tenShortest = filteredDuplicates.sortedBy { it.first.distanceTo(it.second) }.take(10)
-
-    val circuits = mutableListOf<MutableList<JunctionBox>>()
-    val alreadyAdded = mutableListOf<Pair<JunctionBox, JunctionBox>>()
-
-    x@for (connection in tenShortest) {
-      if (circuits.isEmpty()) {
-        alreadyAdded.add(connection)
-        circuits.add(mutableListOf(connection.first, connection.second))
-        continue
-      }
-
-      var circuitsSize = circuits.size
-      var index = 0
-      while (index < circuitsSize) {
-        val circuit = circuits[index]
-        when {
-          alreadyAdded.contains(connection) || alreadyAdded.contains(connection.second to connection.first) -> continue@x
-
-          circuit.contains(connection.first) -> {
-            circuit.add(connection.second)
-            alreadyAdded.add(connection)
-          }
-
-          circuit.contains(connection.second) -> {
-            circuit.add(connection.first)
-            alreadyAdded.add(connection)
-          }
-
-          else -> {
-            alreadyAdded.add(connection)
-            circuits.add(mutableListOf(connection.first, connection.second))
-          }
-        }
-
-        circuitsSize = circuits.size
-        index++
-      }
-    }
-
-    return circuits.take(3).fold(1) { acc, it -> acc * it.size }
+    return sizes.values.sorted().take(3).fold(1) { acc, i -> acc * i }
   }
+
 
   override fun solve2(input: String): Number {
     TODO("Not yet implemented")
